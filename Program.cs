d2a1a83fbb2c91918541b73be910b09b4f50604a
Banshidhar Mishra <mishra.banshidhar81@hotmail.com>
2026-06-02 10:12:16 +0000

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -17,6 +13,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapGet("/", () => "API is running successfully");
 
 app.MapControllers();
 
