cd5b400668e2b9f23cd7e56a531495109514c3d5
mishraba <mishramca2005@gmail.com>
2026-08-25 19:10:03 +0530

    Add FailedRequests and AverageResponseTime properties

--- Controllers/Monitor/MonitoringSummaryDto.cs
+++ Controllers/Monitor/MonitoringSummaryDto.cs
@@ -1,6 +1,8 @@
 public class MonitoringSummaryDto
 {
     public long TotalRequests { get; set; }
+
     public long FailedRequests { get; set; }
+
     public double AverageResponseTime { get; set; }
 }
